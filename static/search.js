94c93640fd5bf9ca0e6c294bd0b850308d71eb1b
Aleksander <lolwolfyou@gmail.com>
2026-05-15 21:49:01 +0200

    refactor: refactored searchbar and main

--- static/search.js
+++ static/search.js
@@ -1,36 +1,75 @@
+import { animate } from "https://cdn.jsdelivr.net/npm/motion@latest/+esm"
+
+
+const modal = document.getElementById("search-modal")
+const openBtn = document.getElementById("open-search")
+openBtn.addEventListener("click", () => {
+  modal.classList.remove("hidden")
+  modal.classList.add("flex")
+  
+  animate(
+    "#search-modal > div",
+    {
+      opacity: [0, 1],
+      y: [-20, 0],
+      scale: [0.98, 1],
+    },
+    {
+      duration: 0.2,
+    }
+  )
+})
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.add("hidden")
+    modal.classList.remove("flex")
+  }
+})
+
 async function setupSearch() {
-  const response = await fetch('/index.json')
+  const response = await fetch("/index.json")
   const pages = await response.json()
 
-  const input = document.getElementById('search')
-  const results = document.getElementById('results')
+  const input = document.getElementById("search")
+  const results = document.getElementById("results")
 
-  input.addEventListener('input', () => {
-
+  input.addEventListener("input", () => {
     const q = input.value.toLowerCase()
 
-    results.innerHTML = ''
+    results.innerHTML = ""
 
     if (!q) return
 
     const matches = pages.filter(page =>
-      page.title.toLowerCase().includes(q) ||
-      page.content.toLowerCase().includes(q)
+      page.title.toLowerCase().includes(q)
     )
-
+  
     matches.forEach(page => {
+
+      const icon =
+        page.section === "blog"
+          ? "ti-user"
+          : "ti-code-dots"
 
       results.innerHTML += `
         <a
           href="${page.permalink}"
-          class="block p-4 border border-zinc-800 rounded-xl hover:bg-zinc-900"
+          class="
+            result-item
+            flex items-center gap-3
+            px-4 py-3 border border-zinc-900
+            group hover:bg-zinc-200 transition
+          "
         >
-          ${page.title}
+          <i class="ti ${icon} text-zinc-900 group-hover:text-zinc-600"></i>
+
+          <span class="group-hover:text-zinc-600">${page.title}</span>
+
+          <i class="ti ti-arrow-right ml-auto text-zinc-900 group-hover:text-zinc-600"></i>
         </a>
       `
-
     })
-
   })
 }
 
